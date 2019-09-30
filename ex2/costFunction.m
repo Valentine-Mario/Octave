aaabfe2ad12a02f0359a8445e380fb3319151024
valentine-mario <valentine13400@gmail.com>
2019-09-30 11:08:44 +0100

    implemented gradiant descent and cost func for logical regression

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -8,7 +8,6 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
@@ -16,6 +15,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+  sigmoid=sigmoid(X*theta);
+  
+  J=-(1/m) * (sum(y' *log(sigmoid) + (1-y)' *log(1-sigmoid)));
+  grad=(1/m)*X'*(sigmoid-y);
+
 %
 % Note: grad should have the same dimensions as theta
 %
